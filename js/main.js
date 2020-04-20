3118907914952266c09df4ff5d5e0625810ad8a0
Rossi Espinoza <aspivina@gmail.com>
2020-04-20 13:06:31 -0300

    version para revisar por el cliente

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 "use strict";
 
+   // Add smooth scrolling to all links
+   $("a").on('click', function(event) {
+		if($(window).width() < 768){
+				$('.menu-mobile').hide();
+		}
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+				// Prevent default anchor click behavior
+				event.preventDefault();
+				// Store hash
+				var hash = this.hash;
+				// Using jQuery's animate() method to add smooth page scroll
+				// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+				$('html, body').animate({
+						scrollTop: $(hash).offset().top
+				}, 800, function(){
+						// Add hash (#) to URL when done scrolling (default click behavior)
+						window.location.hash = hash;
+				});
+		} // End if
+ });
+
+
 //hidding menu elements that do not fit in menu width
 //processing center logo
 function menuHideExtraElements() {
